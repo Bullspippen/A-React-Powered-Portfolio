c208f5d43da2df42cefafceab48cdcf3c7b323e6
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-23 22:11:47 -0400

    resume.js updated and included template, portfolio.js updated with template as well

--- a-react-powered-portfolio/src/components/Portfolio.js
+++ a-react-powered-portfolio/src/components/Portfolio.js
@@ -42,3 +42,43 @@ return (
 
 export default Portfolio
 
+// const Portfolio = () => {
+//     const [projects] = useState([
+//         {
+//             name: 'Note Taker',
+//             description: 'An application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.',
+//             link: "https://note-taker-app-tt.herokuapp.com/",
+//             repo: "https://github.com/example/note-taker",
+//         },
+//         {
+//             name: 'Weather Dashboard',
+//             description: 'An application that allows the user to search for a city and receive current weather and a 5-day forecast. Uses the OpenWeather API to retrieve data.',
+//             link: "https://example.github.io/weather-dashboard/",
+//             repo: "https://github.com/example/weather-dashboard",
+//         },
+//         {
+//             name: 'Employee Directory',
+//             description: 'An employee directory application that allows the user to search for and sort employees. Uses the Random User API to retrieve employee data.',
+//             link: "https://example.github.io/employee-directory/",
+//             repo: "https://github.com/example/employee-directory",
+//         },
+//         {
+//             name: 'Tech Blog',
+//             description: 'A CMS-style blog site where developers can publish their blog posts and comment on other developers’ posts as well. Uses Handlebars.js, Sequelize, and the MVC paradigm.',
+//             link: "https://example-tech-blog.herokuapp.com/",
+//             repo: "https://github.com/example/tech-blog",
+//         },
+//     ]);
+//     return (
+//         <div className="flex-row">
+//             {projects.map((project, i) => (
+//                 <Project
+//                     project={project}
+//                     key={"project" + i}
+//                 />
+//             ))}
+//         </div>
+//     )
+// }
+
+// export default Portfolio
--- a-react-powered-portfolio/src/components/Resume.js
+++ a-react-powered-portfolio/src/components/Resume.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Resume = () => {
+  const [resume] = useState([]);
+
   return (
     <div>
-      
+      {resume.map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
+
+// return (
+//   <section>
+//     <h2>Work Experience</h2>
+//     <div>
+//       <h3>Job 1</h3>
+//       <p>Timelinet</p>
+//       <ul>
+//         <li>Developed and maintained web applications using React and Node.js</li>
+//         <li>Collaborated with a team of designers and developers to create engaging user experiences</li>
+//         <li>Implemented automated testing using Jest and Enzyme</li>
+//       </ul>
+//     </div>
+
+//     <h2>Education</h2>
+//     <div>
+//       <h3>Schoool</h3>
+//       <p>University of ABC</p>
+//       <p>Graduated May 2019</p>
+//     </div>
+
+//     <h2>Skills</h2>
+//     <div>
+//       <ul>
+//         <li>React</li>
+//         <li>Node.js</li>
+//         <li>JavaScript</li>
+//         <li>HTML</li>
+//         <li>CSS</li>
+//       </ul>
+//     </div>
+//   </section>
+// );
