362a1b00c7274f8732e8c58d40ff8659e5ae6488
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-25 22:47:05 -0400

    Refactor project image source and add proptypes - The code in  has been refactored to dynamically import project images and display them. Proptypes have been added to ensure the required properties of the project object are present and of the correct type

--- a-react-powered-portfolio/src/components/Project.js
+++ a-react-powered-portfolio/src/components/Project.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Project = ({project}) => {
-    const{ name, description, link, repo } = project;
+const Project = ({ project }) => {
+  if (!project) {
+    return <div>Project object not found.</div>;
+  }
+
+  const { name, description, link, repo, image } = project;
+
+  if (!name || !description || !link || !repo || !image) {
+    return <div>Project object is missing required properties.</div>;
+  }
+
   return (
     <div className="project" key={name}>
-     {/* <img src={require(`../assets/images/${name}.jpg`).default} alt={name} className="project-bg" />  */}
-     {/* within src folder create assessts, within create images, within images save project images with the same name that are in the project objects */}
-        <div className="project-content">
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <a href={link} target="_blank" rel="noreferrer">View Project</a>
-            <a href={repo} target="_blank" rel="noreferrer">View Repository</a>
-        </div>
+      <img src={require(`../assets/images/${image}`).default} alt={name} className="project-bg" />
+      <div className="project-content">
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <a href={link} target="_blank" rel="noreferrer">
+          View Project
+        </a>
+        <a href={repo} target="_blank" rel="noreferrer">
+          View Repository
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+Project.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    repo: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Project;
