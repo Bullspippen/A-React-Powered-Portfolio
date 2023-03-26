6283cb97e724563e5fcaed634b4459112ba61034
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-25 22:29:04 -0400

    Updated Header.js to include children prop and navigation links with active link styling

--- a-react-powered-portfolio/src/components/Header.js
+++ a-react-powered-portfolio/src/components/Header.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = (props) => {
+const Header = ({ children }) => {
   return (
-    <header className="flex-row space-between px-1">
-      <h1>Mohamed Osman</h1>
-      {props.children}
+    <header className="flex justify-between items-center py-6 px-4 bg-gray-800 text-white">
+      <div>
+        <h1 className="text-2xl font-bold">Mohamed Osman</h1>
+        <p className="text-sm">Web Developer</p>
+      </div>
+      <nav className="space-x-4 text-gray-300 text-sm">
+        <NavLink
+          to="/"
+          exact
+          className="hover:text-white hover:border-white"
+          activeClassName="text-white border-b-2 border-white"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className="hover:text-white hover:border-white"
+          activeClassName="text-white border-b-2 border-white"
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className="hover:text-white hover:border-white"
+          activeClassName="text-white border-b-2 border-white"
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className="hover:text-white hover:border-white"
+          activeClassName="text-white border-b-2 border-white"
+        >
+          Contact
+        </NavLink>
+      </nav>
+      {children}
     </header>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
