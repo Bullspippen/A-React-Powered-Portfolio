fb45e876ecb9731b3efe5336e74665d6acb76a1a
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-26 12:36:22 -0400

    About.js file updated with new profile picture and dependencies - react-bootsrap was installed as well

--- a-react-powered-portfolio/src/components/About.js
+++ a-react-powered-portfolio/src/components/About.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import profilePic from '../assets/profile-pic.jpg';
 
 const About = () => {
   return (
     <section className="my-4">
-      <div className="my-2">
-        <p> BIO </p>
-      </div>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <Image src={profilePic} alt="Profile Picture" roundedCircle fluid />
+          </Col>
+          <Col md={8}>
+            <h2>About Me</h2>
+            <p>Hi there! My name is [Your Name], and I'm a web developer with [X years] of experience in building beautiful and functional websites and applications. I'm passionate about coding and enjoy learning about new technologies and techniques to improve my skills. </p>
+            <p>In my free time, I enjoy [personal interest/hobby] and [personal interest/hobby]. </p>
+            <p>Feel free to contact me to learn more about my work or to collaborate on a project!</p>
+          </Col>
+        </Row>
+      </Container>
     </section>
-  )
+  );
 }
 
-export default About
-
+export default About;
--- a-react-powered-portfolio/src/components/Contact.js
+++ a-react-powered-portfolio/src/components/Contact.js
@@ -1,45 +1,45 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
-// This component renders the Contact page and its content using the ContactForm component and the ContactInfo component
 const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [errors, setErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // This function validates the form fields and returns an object containing any errors
   const validateFields = () => {
     const errors = {};
-    // Validate name field and add error message to errors object if name field is empty
+
     if (!name.trim()) {
       errors.name = 'Name is required';
     }
-    // Validate email field and add error message to errors object if email field is empty or if email field does not contain an @ symbol
+
     if (!email.trim()) {
       errors.email = 'Email is required';
     } else if (!/\S+@\S+\.\S+/.test(email)) {
       errors.email = 'Invalid email format';
     }
-    // Validate message field and add error message to errors object if message field is empty
+
     if (!message.trim()) {
       errors.message = 'Message is required';
     }
+
     return errors;
   };
 
-  // This function handles the form submission and validates the form fields before submitting the form
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Validate form fields and store any errors in the errors object using the validateFields function
+
     const errors = validateFields();
+
     if (Object.keys(errors).length === 0) {
-      // If there are no errors, log a success message to the console and clear the form fields
       console.log('Form submitted successfully');
+      setIsSubmitted(true);
       setName('');
       setEmail('');
       setMessage('');
       setErrors({});
-      // If there are errors, log the errors object to the console and set the errors object to the errors state
     } else {
       setErrors(errors);
     }
@@ -48,50 +48,114 @@ const Contact = () => {
   return (
     <section className="my-4">
       <div className="my-2">
-        <h2 className="text-2xl font-bold">Contact Me</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="my-2">
-            <label htmlFor="name" className="block font-bold text-gray-700 mb-2">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className={`border border-gray-400 p-2 w-full ${errors.name && 'border-red-500'}`}
-            />
-            {errors.name && <p className="text-red-500">{errors.name}</p>}
-          </div>
-          <div className="my-2">
-            <label htmlFor="email" className="block font-bold text-gray-700 mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={`border border-gray-400 p-2 w-full ${errors.email && 'border-red-500'}`}
-            />
-            {errors.email && <p className="text-red-500">{errors.email}</p>}
-          </div>
-          <div className="my-2">
-            <label htmlFor="message" className="block font-bold text-gray-700 mb-2">
-              Message
-            </label>
-            <textarea
-              id="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              className={`border border-gray-400 p-2 w-full h-24 ${errors.message && 'border-red-500'}`}
-            ></textarea>
-            {errors.message && <p className="text-red-500">{errors.message}</p>}
-          </div>
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">
-            Submit
-          </button>
-        </form>
+        <h2 className="text-2xl font-bold mb-2">Contact Me</h2>
+        {isSubmitted ? (
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-center p-4 bg-green-100 rounded"
+          >
+            <p className="text-green-700 font-semibold">Thank you for your message!</p>
+            <p className="text-gray-600">I will get back to you as soon as possible.</p>
+          </motion.div>
+        ) : (
+          <motion.form onSubmit={handleSubmit} className="bg-gray-100 rounded p-4">
+            <div className="my-2">
+              <label htmlFor="name" className="block font-bold text-gray-700 mb-2">
+                Name
+              </label>
+              <motion.input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className={`border border-gray-400 p-2 w-full ${errors.name && 'border-red-500'}`}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.2, duration: 0.5 }}
+              />
+              {errors.name && (
+                <motion.p
+                  initial={{ opacity: 0, x: -10 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                  className="text-red-500"
+                >
+                  {errors.name}
+                </motion.p>
+              )}
+            </div>
+            <div className="my-2">
+              <label htmlFor="email" className="block font-bold text-gray-700 mb-2">
+                Email
+              </label>
+              <motion.input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={`border border-gray-400 p-2 w-full ${errors.email && 'border-red-500'}`}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{ scale: 1.05 }}
+                whileFocus={{ borderColor: '#6366F1' }}
+              />
+              {errors.email && (
+                <motion.p
+                  className="text-red-500"
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                >
+                  {errors.email}
+                </motion.p>
+              )}
+            </div>
+            <div className="my-2">
+              <label htmlFor="message" className="block font-bold text-gray-700 mb-2">
+                Message
+              </label>
+              <motion.textarea
+                id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className={`border border-gray-400 p-2 w-full h-24 ${errors.message && 'border-red-500'}`}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                whileHover={{ scale: 1.05 }}
+                whileFocus={{ borderColor: '#6366F1' }}
+              ></motion.textarea>
+              {errors.message && (
+                <motion.p
+                  className="text-red-500"
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.2, duration: 0.5 }}
+                >
+                  {errors.message}
+                </motion.p>
+              )}
+            </div>
+            <motion.button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              whileHover={{ scale: 1.05 }}
+            >
+              Submit
+            </motion.button>
+          </motion.form>
+        )}
+        {isSubmitted && (
+          <motion.div
+            className="my-2"
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+          >
+            <p className="text-green-500">Thank you for your submission!</p>
+          </motion.div>
+        )}
       </div>
     </section>
   );
