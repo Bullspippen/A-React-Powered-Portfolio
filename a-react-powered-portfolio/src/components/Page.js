367278f080cf3597dc2e4455d558b4ac33590a52
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-25 22:32:39 -0400

    Refactored page.js to use the Navigation component and simplified switch statement for better readability

--- a-react-powered-portfolio/src/components/Page.js
+++ a-react-powered-portfolio/src/components/Page.js
@@ -6,24 +6,29 @@ import Contact from './Contact'
 import Resume from './Resume'
 
 const Page = ({currentPage}) => {
-    const renderPage = () => {
-        switch (currentPage.name) {
-            case 'About Me':
-                return <About />;
-            case 'Portfolio':
-                return <Portfolio />;
-            case 'Contact':
-                return <Contact />;
-            case 'Resume':
-                return <Resume />;
-            default:
-                return <About />;
-        }
-    };
+  const {name} = currentPage;
+
+  const renderPage = () => {
+    switch (name) {
+      case 'About Me':
+        return <About />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+  
   return (
     <section>
-      <h2>{currentPage.name}</h2>
-        <PageContent>{renderPage()}</PageContent>
+      <h2>{name}</h2>
+      <PageContent>
+        {renderPage()}
+      </PageContent>
     </section>
   )
 }
