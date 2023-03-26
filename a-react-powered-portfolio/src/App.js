6c2d4e4452542a4557b1be00ba6f9308f54ca6d1
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-25 23:10:21 -0400

    App.js file updated to import new componenets for About, Portfolio, Contact and Resume - Updated pages array to incude objects with both name and component properties - Modified page componenet to pass the currentPage prop to the relevant component and added setCurrentPage and currentPage props to Navigation component

--- a-react-powered-portfolio/src/App.js
+++ a-react-powered-portfolio/src/App.js
@@ -1,25 +1,30 @@
-import './App.css';
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Page from './components/Page';
 import Footer from './components/Footer';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   const [pages] = useState([
-    { name: 'About Me' },
-    { name: 'Portfolio' },
-    { name: 'Contact' },
-    { name: 'Resume' },
+    { name: 'About Me', component: <About /> },
+    { name: 'Portfolio', component: <Portfolio /> },
+    { name: 'Contact', component: <Contact /> },
+    { name: 'Resume', component: <Resume /> },
   ]);
+
   const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
     <div>
       <Header>
-        <Navigation pages={pages} setCurrentPage={setCurrentPage} currentPage={currentPage}></Navigation>
+        <Navigation pages={pages} setCurrentPage={setCurrentPage} currentPage={currentPage} />
       </Header>
       <main>
-        <Page currentPage={currentPage}></Page>
+        <Page currentPage={currentPage} />
       </main>
       <Footer />
     </div>
