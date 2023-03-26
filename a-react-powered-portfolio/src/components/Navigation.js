63f9b85b23b07af5795e5724c208d92592b440b1
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-26 13:00:41 -0400

    Navigation.js file updated - modified he setCurrentPage() function to pass the entire page object and updated the onClick() function to pass the entire page object

--- a-react-powered-portfolio/src/components/Navigation.js
+++ a-react-powered-portfolio/src/components/Navigation.js
@@ -1,39 +1,35 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react';
 
 // Destructure the props object into its properties and assign them to variables
-const Navigation = (props) => {
-    const {
-        pages=[], 
-        setCurrentPage,
-        currentPage
-    } = props;
-    // Use the useEffect() Hook to update the document title
-    useEffect(() => {
-        document.title = currentPage.name;
-    }, [currentPage]);
+const Navigation = ({ pages = [], setCurrentPage, currentPage }) => {
+  // Use the useEffect() Hook to update the document title
+  useEffect(() => {
+    document.title = `${currentPage.name} | My Portfolio`;
+  }, [currentPage]);
+
   return (
     <nav>
-        {/* // Map over the pages array and render a list item for each page */}
+      {/* Map over the pages array and render a list item for each page */}
       <ul className="flex-row">
-        {pages.map((Page) => (
-            <li
-                className={`mx-5 ${
-                    // If the current page is the same as the page being mapped over, add the navActive class to the list item
-                    currentPage.name === Page.name && 'navActive'
-                }`}
-                key={Page.name}
+        {pages.map((page) => (
+          <li
+            className={`mx-5 ${
+              // If the current page is the same as the page being mapped over, add the navActive class to the list item
+              currentPage.name === page.name && 'navActive'
+            }`}
+            key={page.name}
+          >
+            <span
+              // When the list item is clicked, update the current page
+              onClick={() => setCurrentPage(page)}
             >
-                <span
-                    // When the list item is clicked, update the current page
-                    onClick={() => setCurrentPage(Page.name)}
-                >
-                    {Page.name}
-                </span>
-            </li>
+              {page.name}
+            </span>
+          </li>
         ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
