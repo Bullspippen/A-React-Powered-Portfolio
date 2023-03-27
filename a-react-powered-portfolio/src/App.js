18c2285b6c23569048a1b5bcb24f2a95f45c33f5
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-26 22:32:19 -0400

    app.js, navigation.js and app.css updated - debugging is to be required

--- a-react-powered-portfolio/src/App.js
+++ a-react-powered-portfolio/src/App.js
@@ -3,25 +3,27 @@ import Navigation from './components/Navigation';
 import Page from './components/Page';
 import Footer from './components/Footer';
 
-// This component renders the entire app and its content using the Navigation, Page, and Footer components
 function App() {
-    // Create a state variable for the pages array and assign it an initial value of an array of objects
   const [pages] = useState([
-    { name: 'About Me', },
-    { name: 'Portfolio', },
-    { name: 'Contact', },
-    { name: 'Resume', },
+    { name: 'About Me' },
+    { name: 'Portfolio' },
+    { name: 'Project' },
+    { name: 'Resume' },
   ]);
 
-  // Create a state variable for the current page and assign it an initial value of the first page in the pages array
-  const [currentPage, setCurrentPage] = useState(pages[0].name);
+  const [currentPage, setCurrentPage] = useState(pages[0]);
 
   return (
     <div>
-      {/* // Render the Navigation component and pass it the pages array, the setCurrentPage function, and the currentPage variable */}
-        <Navigation pages={pages} setCurrentPage={setCurrentPage} currentPage={currentPage} />
+      <header className="banner">
+        <h1>My Portfolio</h1>
+      </header>
+      <Navigation
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
       <main>
-        {/* // Render the Page component and pass it the currentPage variable */}
         <Page currentPage={currentPage} />
       </main>
       <Footer />
