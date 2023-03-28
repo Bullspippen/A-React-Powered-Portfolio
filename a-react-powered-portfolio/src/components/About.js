66b9e700316823bc28b1addf8c09cfd984deb3e6
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-27 21:55:17 -0400

    About.js edited bio

--- a-react-powered-portfolio/src/components/About.js
+++ a-react-powered-portfolio/src/components/About.js
@@ -13,11 +13,11 @@ const About = () => {
           <Col md={12}>
             <h2>About Me</h2>
             <p>
-            As a Full Stack Developer, I am driven by a deep passion for
+              As a Full Stack Developer, I am driven by a deep passion for
               technology and the boundless potential of the human mind. My
               expertise in the entire development process, from front-end to
               back-end, is backed by a profound understanding of software
-              architecture and design principles., and I am constantly seeking
+              architecture and design principles, and I am constantly seeking
               to expand my knowledge and skills. My ability to write clean,
               efficient code and to adapt quickly to new technologies has
               allowed me to build robust, user-friendly applications that meet
@@ -29,17 +29,27 @@ const About = () => {
               intellectual exploration. I find solace in introspection and
               self-reflection, and I am constantly striving to deepen my
               understanding of the world and myself. As the philosopher Ralph
-              Waldo Emerson once said, "<strong><em>Life is a journey, not a destination,</em></strong>" and I believe that it is the process of thinking deeply and overcoming challenges that truly defines us.
+              Waldo Emerson once said, "
+              <strong>
+                <em>Life is a journey, not a destination,</em>
+              </strong>
+              " and I believe that it is the process of thinking deeply and
+              overcoming challenges that truly defines us.
             </p>
             <p>
-              Through my work as a Full Stack Developer, I have gained a deep
-              understanding of software architecture and design principles. I am
+              Having worked extensively as a Full Stack Developer, I have
+              developed a comprehensive understanding of the fundamental
+              principles that govern software architecture and design. I am
               committed to innovation and excellence, and I approach every
               challenge with creativity, determination, and a deep desire to
               achieve the best possible outcome. In the words of renowned
-              computer scientist Alan Kay, "<strong><em>The best way to predict the future is to invent it,</em></strong>" and as a Full Stack Developer, I am proud to be part of a community of innovators who are shaping the future of
-              technology and changing the way we live, work, and connect with
-              one another.
+              computer scientist Alan Kay, "
+              <strong>
+                <em>The best way to predict the future is to invent it,</em>
+              </strong>
+              " and as a Full Stack Developer, I am proud to be part of a
+              community of innovators who are shaping the future of technology
+              and changing the way we live, work, and connect with one another.
             </p>
             <p>
               As an individual, I must acknowledge and express my admiration for
@@ -49,7 +59,14 @@ const About = () => {
               demonstrated an admirable level of fortitude and steadfastness.
               The ability to persevere through challenges and find solutions is
               a hallmark of success, as noted by the esteemed psychologist
-              Albert Bandura, who stated, "<strong><em>Self-belief does not necessarily ensure success, but self-disbelief assuredly spawns failure.</em></strong>"
+              Albert Bandura, who stated, "
+              <strong>
+                <em>
+                  Self-belief does not necessarily ensure success, but
+                  self-disbelief assuredly spawns failure.
+                </em>
+              </strong>
+              "
             </p>
             <p>
               In all aspects of my life, whether personal or professional, I
@@ -57,18 +74,30 @@ const About = () => {
               self-discovery. As a Full Stack Developer, I am proud to be part
               of a community of trailblazers and innovators who are shaping the
               future of technology and transforming the world around us. The
-              words of Alan Kay, who once said, "<strong><em>The best way to predict the
-              future is to invent it,</em></strong>" inspire me to continue pushing the
-              boundaries of what is possible, and to embrace the journey of
-              intellectual discovery that lies before us all.
+              words of Alan Kay, who once said, "
+              <strong>
+                <em>The best way to predict the future is to invent it,</em>
+              </strong>
+              " inspire me to continue pushing the boundaries of what is
+              possible, and to embrace the journey of intellectual discovery
+              that lies before us all.
             </p>
             <p>
               My love for intellectual discovery and the power of the human mind
               aligns with the words of Aristotle, the renowned philosopher, who
-              once said, "<strong><em>Whosoever is delighted in solitude is either a wild
-              beast or a god.</em></strong>" I am reminded of the great scholar and
-              mathematician Archimedes, who shouted, "<strong><em>Eureka! I have found it!</em></strong>"
-              after discovering a solution to a challenging problem while
+              once said, "
+              <strong>
+                <em>
+                  Whosoever is delighted in solitude is either a wild beast or a
+                  god.
+                </em>
+              </strong>
+              " I am reminded of the great scholar and mathematician Archimedes,
+              who shouted, "
+              <strong>
+                <em>Eureka! I have found it!</em>
+              </strong>
+              " after discovering a solution to a challenging problem while
               bathing in solitude.
             </p>
           </Col>
