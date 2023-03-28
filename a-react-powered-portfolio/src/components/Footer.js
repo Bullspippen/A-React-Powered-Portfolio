6ca722b0f17e935a7067432c2da793221e5ecce1
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-27 21:59:16 -0400

    Footer.js file updated to bold the footer section that contains city, province, email, copyright, date

--- a-react-powered-portfolio/src/components/Footer.js
+++ a-react-powered-portfolio/src/components/Footer.js
@@ -6,34 +6,33 @@ const Footer = () => {
   return (
     <footer className="fixed-bottom bg-gray-900 text-center">
       <Container>
-      <div className="">
-        <div className="">
-          <p className="">
-            Toronto, Ontario
-            <br />
-            Email: mohamed.osman111681@gmail.com
-          </p>
-        </div>
         <div className="">
           <div className="">
-            <p>&copy; {new Date().getFullYear()} Mohamed Osman</p>
+            <p className="">
+              <strong>Toronto, Ontario</strong><br />
+              <strong>Email:</strong> mohamed.osman111681@gmail.com
+            </p>
           </div>
           <div className="">
-            <a
-              href="https://www.linkedin.com/in/mohamedosman111681/"
-              className=""
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://github.com/mohamed-osman"
-              className=""
-            >
-              <FaGithub />
-            </a>
+            <div className="">
+              <p><strong>&copy; {new Date().getFullYear()} Mohamed Osman</strong></p>
+            </div>
+            <div className="">
+              <a
+                href="https://www.linkedin.com/in/mohamedosman111681/"
+                className=""
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href="https://github.com/mohamed-osman"
+                className=""
+              >
+                <FaGithub />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
       </Container>
     </footer>
   );
